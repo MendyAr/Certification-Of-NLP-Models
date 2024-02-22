ada58125fa044e984f8c7d70540f0bf507d5aff0
Dror.Tomer <dror.tomer.il@gmail.com>
2024-02-22 11:56:26 +0200

    user add request

--- Scheduler.py
+++ Scheduler.py
@@ -1,18 +1,25 @@
 import Request
 import EvaluationEngine
+import Agent
 
 class Scheduler:
     def __init__(self):
-        # list of requests
-        # request Agent
-        # eval_engine
+        self.users_requests_list = [] # for each : users: set, Request, start time, score
+        self.agent_requests_list = [] # Requests object
+        self.user_requests_counter = 0
+        self.users_2_agent_ratio = 10 # for num of request eval agent req
+        self.agent_min_restock_requests = 10 # len(agent_requests_list) < this val then restock
+        self.agent = Agent()
+        self.eval_engine = EvaluationEngine()
         pass
 
-    def get_(self,eval_request : Request):
-        pass
-
-    def add_request(self,eval_request : Request):
-        pass
+    def add_request(self, eval_request : Request, user_name):
+        foundit = None
+        for ur in self.users_requests_list:
+            if ur.request == eval_request:
+                if not user_name in ur[0]:
+                    ur[0].add(user_name)
+                    self.sort_requests_list()
 
     def next_request(self):
         # remove the next eval request and return it
@@ -30,3 +37,17 @@ class Scheduler:
         pass
 
 
+
+class UserRequest:
+    def __init__(self, users, request : Request, starttime, score : float):
+        self.users = users
+        self.request = request
+        self.starttime = starttime
+        self.score = score
+    
+    def __eq__(self, __value: object) -> bool:
+        if type(__value) != UserRequest:
+            return False
+        if self.request == __value.request:
+            return True
+        return False
