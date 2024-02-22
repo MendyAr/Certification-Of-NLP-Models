f63eb21bfa56edf4234d8f7dd4277f17f3c21cba
Dror.Tomer <dror.tomer.il@gmail.com>
2024-02-22 11:09:08 +0200

    more starter code

--- UserHandler.py
+++ UserHandler.py
@@ -1,11 +1,18 @@
 class UserHandler:
     def __init__(self):
+        # schedualer
+        # storage
         pass
 
-    def connect(self):
+    def login(self,name):
         # Implement connection functionality
         pass
 
-    def disconnect(self):
+    def logout(self,name):
         # Implement disconnect functionality
+        pass
+
+    def main_loop(self):
+        # login
+        # option - 
         pass
--- main.py
+++ main.py
@@ -5,14 +5,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# # Example usage:
-# scheduler = Scheduler()
-# storage = Storage()
-# eval_engine = EvaluationEngine()
-
-# # This would be a sequence of operations, for example:
-# user = front.user_login()
-# request = scheduler.eval_request('some_request')
-# results = eval_engine.run_request(request)
-# storage.results.append(results)
