4c9b581210f98ca60e23749c464ad206c73f9ccd
shirlavi <shirush40@gmail.com>
2024-03-02 20:55:33 +0200

    add storage to be singelton and add save function in scheduler

--- EvaluationEngine.py
+++ EvaluationEngine.py
@@ -1,13 +1,20 @@
 import Request
 import random
+
+from Result import Result
+from Storage import Storage
+
+
 class EvaluationEngine:
     def __init__(self):
+        self.storage = Storage.get_instance()
         pass
 
     def run_eval_request(self, request : Request):
         # Implement request running
         # run req
         score = random.random()
+
         # save the data
         self.save(score,request)
         return score
@@ -16,4 +23,7 @@ class EvaluationEngine:
 
     def save(self,score,request):
         # Implement save functionality
+        res = Result(request, score) #need to change beacuse it update result and not add
+        self.storage.add_result(res); #same
+        self.storage.save_results_to_file(); #same
         pass
