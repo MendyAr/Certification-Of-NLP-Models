3ebe1256b888c20a4e58d0f3415d60175d5a33fc
shirlavi <shirush40@gmail.com>
2024-05-14 20:19:13 +0300

    add home page

--- frontend/src/pages/HomePage/MainPageTable/MainPageTable.tsx
+++ frontend/src/pages/HomePage/MainPageTable/MainPageTable.tsx
@@ -2,36 +2,52 @@ import { Table, TableProps } from "antd";
 import React from "react";
 
 export default function MainPageTable() {
-  function generateColumns() {
-    //const columns: TableProps["columns"] = [];
-    const columns: { title: string; dataIndex: string; key: string }[] = [];
+    function generateColumns() {
+        //const columns: TableProps["columns"] = [];
+        const columns: { title: string; dataIndex: string; key: string }[] = [];
 
-    columns.push(
-      {
-        title: "Name",
-        dataIndex: "name",
-        key: "name",
-      },
-      {
-        title: "Age",
-        dataIndex: "age",
-        key: "age",
-      }
+        columns.push(
+            {
+                title: "Model",
+                dataIndex: "model",
+                key: "model",
+            },
+            {
+                title: "Questionnaire",
+                dataIndex: "questionnaire",
+                key: "questionnaire",
+            },
+            {
+                title: "Result",
+                dataIndex: "result",
+                key: "result",
+            }
+        );
+
+        return columns;
+    }
+
+    return (
+        <Table
+            columns={generateColumns()}
+            dataSource={[
+                {
+                    model: "NLP1",
+                    questionnaire: "ASI",
+                    result: "0.8",
+                },
+                {
+                    model: "NLP2",
+                    questionnaire: "BIG5",
+                    result: "0.56",
+                },
+                {
+                    model: "NLP3",
+                    questionnaire: "ASI",
+                    result: "0.9",
+                },
+            ]}
+            style={{ width: "100%", height: "100%" }}
+        />
     );
-
-    return columns;
-  }
-
-  return (
-    <Table
-      columns={generateColumns()}
-      dataSource={[
-        {
-          age: 1,
-          name: "John",
-        },
-      ]}
-      style={{ width: "100%", height: "100%" }}
-    />
-  );
 }
