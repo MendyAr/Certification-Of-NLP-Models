748624635f35de8f27c273232c91973f8ba7d34e
mendi <mendiaharonian@gmail.com>
2024-07-09 19:32:58 +0300

    - resolving bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import { router } from "./router/router";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import { ConfigProvider as AntdTheme, theme } from "antd";
-require('react-dotenv').config();
 
 function App() {
   const { isLight } = useSelector((state: RootState) => state.theme);
